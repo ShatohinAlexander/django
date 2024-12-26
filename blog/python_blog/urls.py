6339f06095e36a8323a43e17fd9e4c0e820a193d
ShatohinAlexander <saatohinaleksandr@gmail.com>
2024-12-26 14:55:57 +0700

    hw_22 Создание base.html и папки static. Корректировка url приложения по замечаниям к hw_21

--- blog/python_blog/urls.py
+++ blog/python_blog/urls.py
@@ -4,12 +4,12 @@ from python_blog.views import catalog_posts,post_detail,catalog_categories,categ
 app_name = 'blog'
 
 urlpatterns = [
-    path('', catalog_posts, name = 'posts'),
+    path('/', catalog_posts, name = 'posts'),
 # Категории
     path('categories/', catalog_categories, name = 'categories'),
-    path('categories/<slug:category_slug>', category_detail, name = 'category_detail'),
+    path('categories/<slug:category_slug>/', category_detail, name = 'category_detail'),
 # Теги
-    path('tags', catalog_tags, name = 'tags'),
+    path('tags/', catalog_tags, name = 'tags'),
     path('tags/<slug:tag_slug>/', tag_detail, name = 'tags_detail'),
 # Посты
     path('<slug:post_slug>/', post_detail, name = 'post_detail'),
