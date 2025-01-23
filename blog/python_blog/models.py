e59ae6ed80c76f639b65c43033333b0d629b604e
ShatohinAlexander <alexander.shatohin95@mail.ru>
2025-01-23 23:39:46 +0700

    hw_24 Изменение админки, допилил модели и пересоздал миграции

--- blog/python_blog/models.py
+++ blog/python_blog/models.py
@@ -12,14 +12,23 @@ class Tag(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
     slug = models.SlugField(unique=True, blank=True, verbose_name='URL')
 
-class Post(models.Model):    
-    title = models.CharField(max_length=200, verbose_name='Заголовок')
-    content = models.TextField(verbose_name='Содержание')
-    slug = models.SlugField(unique=True, blank=True, verbose_name='URL')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', null=True, blank=True, verbose_name='Категория')
-    tags = models.ManyToManyField(Tag, related_name='posts', null=True, blank=True, verbose_name='Теги')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_date = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержание", null=True)
+    slug = models.SlugField(unique=True, blank=True, verbose_name="URL")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        null=True,
+        blank=True,
+        verbose_name="Категория",
+    )
+    tags = models.ManyToManyField(
+        Tag, related_name="posts", blank=True, verbose_name="Теги"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_date = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -28,7 +37,7 @@ class Post(models.Model):
         super().save(*args, **kwargs)
 
     def str(self):
-        return self.title
-    
+        return str(self.title)
+
     def get_absolute_url(self):
-        return reverse('blog:post_detail', kwargs={'post_slug': self.slug})    
+        return reverse("blog:post_detail", kwargs={"post_slug": self.slug})  
